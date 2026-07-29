3b3e3e5714ec386c10814ba192e80bf40e0af0af
Marcela <marcela.wojtasik@alu.inspt.utn.edu.ar>
2026-07-29 11:19:24 -0300

    AppConfig

--- src/gestionhospitalaria/AppConfig.java
+++ src/gestionhospitalaria/AppConfig.java
@@ -1,5 +1,64 @@
 package gestionhospitalaria;
 
+import views.CamaView;
+import views.MedicoView;
+import views.PacienteView;
+import views.ReservaView;
+import views.AsignacionView;
+import utils.Mensajes;
+import utils.Mostrar;
+import java.util.Scanner;
+
 public class AppConfig {
 
+    public static void iniciarSistema() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        PacienteView pacienteView = new PacienteView(scanner);
+        MedicoView medicoView = new MedicoView(scanner);
+        CamaView camaView = new CamaView(scanner);
+        ReservaView reservaView = new ReservaView(scanner);
+        AsignacionView asignacionView = new AsignacionView(scanner);
+
+        do {
+            String menuPrincipal = """
+                                   
+                                   --- SISTEMA DE GESTION HOSPITALARIA ---
+                                   1. Gestión de Pacientes
+                                   2. Gestión de Médicos
+                                   3. Gestión de Camas
+                                   4. Gestión de Reservas
+                                   5. Gestión de Asignaciones (Médico - Paciente)
+                                   0. Salir""";
+
+            opcion = Mostrar.Menu(menuPrincipal, scanner);
+
+            switch (opcion) {
+                case 1:
+                    pacienteView.mostrarMenu();
+                    break;
+                case 2:
+                    medicoView.mostrarMenu();
+                    break;
+                case 3:
+                    camaView.mostrarMenu();
+                    break;
+                case 4:
+                    reservaView.mostrarMenu();
+                    break;
+                case 5:
+                    asignacionView.mostrarMenu();
+                    break;
+                case 0:
+                    Mostrar.Mensaje(Mensajes.SALIENDO);
+                    break;
+                case -1:
+                default:
+                    Mostrar.Mensaje(Mensajes.OPCION_INVALIDA);
+            }
+        } while (opcion != 0);
+
+        scanner.close();
+    }
 }
--- src/gestionhospitalaria/Main.java
+++ src/gestionhospitalaria/Main.java
@@ -1,10 +1,16 @@
 package gestionhospitalaria;
 
+import static gestionhospitalaria.AppConfig.iniciarSistema;
 
 public class Main {
 
     public static void main(String[] args) {
         
+        //Configuro codificación del sistema con UTF-8
+        System.setOut(new java.io.PrintStream(System.out, true, java.nio.charset.StandardCharsets.UTF_8));
+
+        iniciarSistema();
+
     }
 
 }
